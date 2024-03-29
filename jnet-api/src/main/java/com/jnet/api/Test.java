9173a023ef2eaaf6e0f0fc28908b05df9ff0bfa2
root <mgwmgwmgw@126.com>
2024-03-29 14:30:41 +0800

    init

---
+++ jnet-api/src/main/java/com/jnet/api/Test.java
@@ -0,0 +1,17 @@
+package com.jnet.api;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author mugw
+ * @version 1.0
+ * @description
+ * @date 2024/2/20 10:56:21
+ */
+@Slf4j
+public class Test {
+
+    public void ss(){
+        log.error("{}","ss");
+    }
+}
